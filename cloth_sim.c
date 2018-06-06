0509e6ef1b0c7b661c56b47125e6411a36e56e65
eddie5210200 <eddie5210200@yahoo.com>
2018-06-06 16:48:49 -0700

    Add files via upload

--- cloth_sim.c
+++ cloth_sim.c
@@ -10,51 +10,52 @@
   #include <GL/glut.h> 
 #endif
 
+
+
+
 #define BALL_POSITION_X 6
 #define BALL_POSITION_Y -7
 #define BALL_POSITION_Z 0
-#define BALL_RADIUS 2
+#define BALL_RADIUS 0.75
 #define TRUE 1
 #define FALSE 0
-#define PARTICLE_NUMBER_X 100
-#define PARTICLE_NUMBER_Y 100
+#define PARTICLE_NUMBER_X 50
+#define PARTICLE_NUMBER_Y 50
 #define SPRING_NUMBER (PARTICLE_NUMBER_X) * (PARTICLE_NUMBER_Y - 1) + (PARTICLE_NUMBER_X - 1) * (PARTICLE_NUMBER_Y) + (PARTICLE_NUMBER_X - 1) * (PARTICLE_NUMBER_Y - 1) * 2
-#define SPRING_LENGTH 0.12
-#define SPRING_CONSTANT 0.2
+#define SPRING_LENGTH 0.25
+#define SPRING_CONSTANT 0.05
 #define EXCEPTION_NUMBER 6
 
 //Use w to switch from wireframe to cloth
-//Run in wireframe to reduce lag
-//Space to run
 
 // *************************************************************************************
 // * GLOBAL variables. Not ideal but necessary to get around limitatins of GLUT API... *
 // *************************************************************************************
 int pause = TRUE;
-int wire = FALSE;
+int wire = TRUE;
 typedef struct Vec2{
     int x;
     int y;
 } vec2;
 typedef struct Vector{
-    double x;
-    double y;
-    double z;
+    float x;
+    float y;
+    float z;
 }Vector;
 
 typedef struct Particle{
     Vector pos;
     Vector velocity;
     Vector forces;
-    double mass;
-    double radius;
+    float mass;
+    float radius;
 } Particle;
 
 typedef struct Spring{
     vec2 end1;
     vec2 end2;
-    double k;
-    double initialLength;
+    float k;
+    float initialLength;
 } Spring;
 
 typedef struct Verlet{
@@ -62,25 +63,13 @@ typedef struct Verlet{
     Vector oldPos[PARTICLE_NUMBER_Y][PARTICLE_NUMBER_X];
     Vector forces[PARTICLE_NUMBER_Y][PARTICLE_NUMBER_X];
     Vector gravity;
-    double dt;
+    float dt;
 } Verlet;
 
 Verlet verlet;
 Particle particle[PARTICLE_NUMBER_Y][PARTICLE_NUMBER_X];
 Spring springs[SPRING_NUMBER];
 vec2 exceptions[EXCEPTION_NUMBER];
-Vector ball;
-
-//variables for calculations - for faster computation
-Vector position2;
-Vector position1;
-Vector difference_position, correction; 
-double displacement, f, unit;  
-Vector force;     
-vec2 j;
-Vector particle_to_ball;
-double total_dist;
-Vector current, temp, old, a;
 
 void init (void)
 {
@@ -109,15 +98,15 @@ void init (void)
 
   for(int i = 0; i < PARTICLE_NUMBER_Y; i++){
      for(int j = 0; j < PARTICLE_NUMBER_X; j++){
-        particle[i][j].pos.x = i * SPRING_LENGTH;
-        verlet.oldPos[i][j].x = i * SPRING_LENGTH;
-        verlet.newPos[i][j].x = i * SPRING_LENGTH;
-        particle[i][j].pos.y = 1 - j * SPRING_LENGTH;
-        verlet.oldPos[i][j].y = 1 - j * SPRING_LENGTH;
-        verlet.newPos[i][j].y = 1 - j * SPRING_LENGTH;
-        particle[i][j].pos.z = 0;
-        verlet.oldPos[i][j].z = 0;
-        verlet.newPos[i][j].z = 0;
+        particle[i][j].pos.x = BALL_POSITION_X - 6 + i * SPRING_LENGTH;
+        verlet.oldPos[i][j].x = BALL_POSITION_X - 6 + i * SPRING_LENGTH;
+        verlet.newPos[i][j].x = BALL_POSITION_X - 6 + i * SPRING_LENGTH;
+        particle[i][j].pos.y = BALL_POSITION_Y + 8 - j * SPRING_LENGTH;
+        verlet.oldPos[i][j].y = BALL_POSITION_Y + 8 - j * SPRING_LENGTH;
+        verlet.newPos[i][j].y = BALL_POSITION_Y + 8 - j * SPRING_LENGTH;
+        particle[i][j].pos.z = BALL_POSITION_Z;
+        verlet.oldPos[i][j].z = BALL_POSITION_Z;
+        verlet.newPos[i][j].z = BALL_POSITION_Z;
         particle[i][j].forces.x = 0;
         particle[i][j].forces.y = 0;
         particle[i][j].forces.z = 0;
@@ -131,12 +120,12 @@ void init (void)
         particle[i][j].radius = 0.15;
     }
   }
-  Vector particle_to_particle, particle_to_particle_2;
   //vertical/horizontal springs
   int spring_index = 0;
   for(int i = 0; i < PARTICLE_NUMBER_Y - 1; i++){
      for(int j = 0; j < PARTICLE_NUMBER_X; j++){
         //Horizontal Springs
+        Vector particle_to_particle;
         particle_to_particle.x = particle[i + 1][j].pos.x - particle[i][j].pos.x;
         particle_to_particle.y = particle[i + 1][j].pos.y - particle[i][j].pos.y;
         particle_to_particle.z = particle[i + 1][j].pos.z - particle[i][j].pos.z;
@@ -148,6 +137,7 @@ void init (void)
         springs[spring_index].initialLength = sqrt(pow(particle_to_particle.x, 2) + pow(particle_to_particle.y, 2) + pow(particle_to_particle.z, 2));
         spring_index++;
         //Vertical Springs
+        Vector particle_to_particle_2;
         particle_to_particle_2.x = particle[j][i + 1].pos.x - particle[j][i].pos.x;
         particle_to_particle_2.y = particle[j][i + 1].pos.y - particle[j][i].pos.y;
         particle_to_particle_2.z = particle[j][i + 1].pos.z - particle[j][i].pos.z;
@@ -160,11 +150,11 @@ void init (void)
         spring_index++;
     }
   }
- 
   //shear springs
   for(int i = 0; i < PARTICLE_NUMBER_Y - 1; i++){
      for(int j = 0; j < PARTICLE_NUMBER_X - 1; j++){
         //Horizontal Springs
+        Vector particle_to_particle;
         particle_to_particle.x = particle[i][j].pos.x - particle[i + 1][j + 1].pos.x;
         particle_to_particle.y = particle[i][j].pos.y - particle[i + 1][j + 1].pos.y;
         particle_to_particle.z = particle[i][j].pos.z - particle[i + 1][j + 1].pos.z;
@@ -176,6 +166,7 @@ void init (void)
         springs[spring_index].initialLength = sqrt(pow(particle_to_particle.x, 2) + pow(particle_to_particle.y, 2) + pow(particle_to_particle.z, 2));
         spring_index++;
         //Vertical Springs
+        Vector particle_to_particle_2;
         particle_to_particle_2.x = particle[i + 1][j].pos.x - particle[i][j + 1].pos.x;
         particle_to_particle_2.y = particle[i + 1][j].pos.y - particle[i][j + 1].pos.y;
         particle_to_particle_2.z = particle[i + 1][j].pos.z - particle[i][j + 1].pos.z;
@@ -189,7 +180,7 @@ void init (void)
     }
   }
   verlet.gravity.x = 0;
-  verlet.gravity.y = -10;
+  verlet.gravity.y = -100;
   verlet.gravity.z = 0;
 
   //anchor points
@@ -225,36 +216,33 @@ void init (void)
       }
       exceptions[i] = temp;
    }
-
-    //setup ball
-    ball.x = BALL_POSITION_X + 5;
-    ball.y = BALL_POSITION_Y;
-    ball.z = BALL_POSITION_Z + 3;
 }
 
-void compute_detection(Vector circle, double radius){
-    for(int i = 0; i < SPRING_NUMBER; i++){
-        j = springs[i].end1;
+/*
+void compute_detection(){
+    Vector particle_to_ball;
+    float total_dist;
+        for(int i = 0; i < PARTICLE_NUMBER; i++){
         //calculate the difference between circle center and point
-        particle_to_ball.x = verlet.newPos[j.y][j.x].x - circle.x;
-        particle_to_ball.y = verlet.newPos[j.y][j.x].y - circle.y;
-        particle_to_ball.z = verlet.newPos[j.y][j.x].z - circle.z;
+        particle_to_ball.x = verlet.newPos[i].x - BALL_POSITION_X;
+        particle_to_ball.y = verlet.newPos[i].y - BALL_POSITION_Y;
+        particle_to_ball.z = verlet.newPos[i].z - BALL_POSITION_Z;
         total_dist = sqrt(pow(particle_to_ball.x, 2) + pow(particle_to_ball.y, 2) + pow(particle_to_ball.z, 2));
         //normalize and offset from the circle
-        if(total_dist < radius){
+        if(total_dist < particle[i].radius + BALL_RADIUS){
             Vector normalize;
             normalize.x = particle_to_ball.x/total_dist;
             normalize.y = particle_to_ball.y/total_dist;
             normalize.z = particle_to_ball.z/total_dist;
-            normalize.x *= (radius - total_dist);
-            normalize.y *= (radius - total_dist);
-            normalize.z *= (radius - total_dist);
-            verlet.newPos[j.y][j.x].x += normalize.x;
-            verlet.newPos[j.y][j.x].y += normalize.y;
-            verlet.newPos[j.y][j.x].z += normalize.z;
+            normalize.x *= (BALL_RADIUS - total_dist);
+            normalize.y *= (BALL_RADIUS - total_dist);
+            normalize.z *= (BALL_RADIUS - total_dist);
+            verlet.newPos[i].x += normalize.x;
+            verlet.newPos[i].y += normalize.y;
+            verlet.newPos[i].z += normalize.z;
         }
     }
-}
+}*/
 
 int exception(int y, int x){
     for(int i = 0; i < EXCEPTION_NUMBER; i++)
@@ -262,9 +250,14 @@ int exception(int y, int x){
     return 0;
 }
 
-
 void satisfy_condition(void){
     for(int i = 0; i < SPRING_NUMBER; i++){
+        Vector position2;
+        Vector position1;
+        Vector correction;
+        Vector difference_position;
+        float dist, f;      
+        vec2 j;
         //calculate spring forces k * (L - l)
         j = springs[i].end1;
         position1.x = verlet.newPos[j.y][j.x].x;
@@ -277,10 +270,10 @@ void satisfy_condition(void){
         difference_position.x = position2.x - position1.x; 
         difference_position.y = position2.y - position1.y;
         difference_position.z = position2.z - position1.z;
-        displacement = sqrt(pow(difference_position.x, 2) + pow(difference_position.y, 2) + pow(difference_position.z, 2));
-        correction.x = difference_position.x * (1.0f - springs[i].initialLength / displacement);
-        correction.y = difference_position.y * (1.0f - springs[i].initialLength / displacement);
-        correction.z = difference_position.z * (1.0f - springs[i].initialLength / displacement);
+        dist = sqrt(pow(difference_position.x, 2) + pow(difference_position.y, 2) + pow(difference_position.z, 2));
+        correction.x = difference_position.x * (1.0f - springs[i].initialLength / dist);
+        correction.y = difference_position.y * (1.0f - springs[i].initialLength / dist);
+        correction.z = difference_position.z * (1.0f - springs[i].initialLength / dist);
             
         j = springs[i].end1;
         if(!exception(j.y, j.x)){    
@@ -302,19 +295,25 @@ void satisfy_condition(void){
 void calcLoads(void){
     for(int i = 0; !pause && i < PARTICLE_NUMBER_Y; i++){
         for(int j = 0; j < PARTICLE_NUMBER_X; j++){
+            //zero forces
+            verlet.forces[i][j].x = 0;
+            verlet.forces[i][j].y = 0;
+            verlet.forces[i][j].z = 0;
             //gravity
             verlet.forces[i][j].x += verlet.gravity.x;
             verlet.forces[i][j].y += verlet.gravity.y;
             verlet.forces[i][j].z += verlet.gravity.z;
-            //wind forces
-            verlet.forces[i][j].x += 0;
-            verlet.forces[i][j].y += 0;
-            verlet.forces[i][j].z += -20;
         }
     }    
 
     //calculate spring forces
     for(int i = 0; !pause && i < SPRING_NUMBER; i++){
+            Vector position2;
+            Vector position1;
+            Vector difference_position; 
+            float displacement, f;  
+            Vector force;     
+            vec2 j;
             //calculate spring forces k * (L - l)
             j = springs[i].end1;
             position1.x = verlet.newPos[j.y][j.x].x;
@@ -327,7 +326,8 @@ void calcLoads(void){
             difference_position.x = position2.x - position1.x; 
             difference_position.y = position2.y - position1.y;
             difference_position.z = position2.z - position1.z;
-            unit = sqrt(pow(difference_position.x, 2) + pow(difference_position.y, 2) + pow(difference_position.z, 2));
+            float unit = sqrt(pow(difference_position.x, 2) + pow(difference_position.y, 2) + pow(difference_position.z, 2));
+
             displacement = unit - springs[i].initialLength;
             f = displacement * springs[i].k;
             
@@ -344,9 +344,7 @@ void calcLoads(void){
             verlet.forces[j.y][j.x].y -= force.y/2.0f;
             verlet.forces[j.y][j.x].z -= force.z/2.0f;
     }
-
-    //satisfy conditions
-    for(int i = 0; i < 15 && !pause; i++)
+    for(int i = 0; i < 100 && !pause; i++)
         satisfy_condition();
 
     //integrate equations of motion
@@ -354,12 +352,13 @@ void calcLoads(void){
         for(int j = 0; j < PARTICLE_NUMBER_X; j++){
             //skip for anchor points
             if(exception(i, j)) continue;
-            current = verlet.newPos[i][j];
+            Vector current = verlet.newPos[i][j];
+            Vector temp;
             temp.x = current.x;
             temp.y = current.y;
             temp.z = current.z;
-            old = verlet.oldPos[i][j];
-            a = verlet.forces[i][j];
+            Vector old = verlet.oldPos[i][j];
+            Vector a = verlet.forces[i][j];
             verlet.newPos[i][j].x += (current.x - old.x) + a.x * verlet.dt * verlet.dt;
             verlet.newPos[i][j].y += (current.y - old.y) + a.y * verlet.dt * verlet.dt;
             verlet.newPos[i][j].z += (current.z - old.z) + a.z * verlet.dt * verlet.dt;
@@ -374,10 +373,10 @@ void calcLoads(void){
 
 void draw_rope(void){
     calcLoads();
-    compute_detection(ball, BALL_RADIUS);
     if(wire){
         for(int i = 0; i < PARTICLE_NUMBER_Y; i++){
             for(int j = 0; j < PARTICLE_NUMBER_X; j++){
+                //compute_detection();
                 glPushMatrix();
                 //Circle    
                 glColor3f(1.0f, 1.0f, 1.0f);
@@ -400,29 +399,26 @@ void draw_rope(void){
     } else {
         //draw cloth
         for(int i = 0; i < PARTICLE_NUMBER_Y - 1; i++){
-            glPushMatrix();
-            glBegin(GL_TRIANGLE_STRIP);       
-            for(int j = 0; j < PARTICLE_NUMBER_X; j++){ 
-                //glTranslatef(verlet.newPos[i][j].x, verlet.newPos[i][j].y, verlet.newPos[i][j].z);
-                if(j % 2 == 0 && i % 2 == 0) glColor3f(1.0f, 1.0f, 1.0f);
+            for(int j = 0; j < PARTICLE_NUMBER_X - 1; j++){
+                glPushMatrix();
+                //Circle    
+                if(j % 2 == 0) glColor3f(1.0f, 1.0f, 1.0f);
                 else glColor3f(0.5f, 0.5f, 0.5f);
-                glVertex3f(verlet.newPos[i][j].x, verlet.newPos[i][j].y, verlet.newPos[i][j].z);  
-                glVertex3f(verlet.newPos[i + 1][j].x, verlet.newPos[i + 1][j].y, verlet.newPos[i + 1][j].z);   
+                glTranslatef(verlet.newPos[i][j].x, verlet.newPos[i][j].y, verlet.newPos[i][j].z);
+                glBegin(GL_TRIANGLE_FAN);
+                glVertex3f(0, 0, 0);
+                glVertex3f(SPRING_LENGTH, 0, 0);
+                glVertex3f(SPRING_LENGTH, SPRING_LENGTH, 0);
+                glVertex3f(0, SPRING_LENGTH, 0);
+                glEnd();
+                glPopMatrix();
             }
-            glEnd();
-            glPopMatrix();
         }    
+
     }
 }
 
-void moveball(void){
-   if(!pause){
-       if(ball.z > -20){
-            ball.z-= 0.2;
-       } 
-       
-   }
-}       
+
 
 void display (void)
 {
@@ -440,18 +436,16 @@ void display (void)
   glEnd ();
   glEnable (GL_LIGHTING);
   glTranslatef (-6.5, 6, -9.0f); // move camera out and center on the rope
-  //draw_rope();
+  draw_rope();
   glPushMatrix ();
-  glTranslatef (ball.x, ball.y, ball.z);
-  glColor3f (1.0f, 1.0f, 0.0f);
-  glutSolidSphere (BALL_RADIUS - 0.1, 50, 50); 
+  //glTranslatef (BALL_POSITION_X, BALL_POSITION_Y, BALL_POSITION_Z);
+  //glColor3f (1.0f, 1.0f, 0.0f);
+  //glutSolidSphere (BALL_RADIUS - 0.1, 50, 50); 
 // draw the ball, but with a slightly lower radius, otherwise we could get ugly visual artifacts of rope penetrating the ball slightly
   glPopMatrix ();
   draw_rope();
-  moveball();
   glutSwapBuffers();
   glutPostRedisplay();
-  //printf("%3f\n", ball.z);
 }
 
 
